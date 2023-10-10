ab620e2987c4910352468151fed6c90bd7f9b484
BRENO <110254486+Br3n0L@users.noreply.github.com>
2023-10-09 22:10:56 -0300

    FINAL

--- imc_calculator_flutter/lib/pages/home_page.dart
+++ imc_calculator_flutter/lib/pages/home_page.dart
@@ -12,112 +12,73 @@ class _HomePageState extends State<HomePage> {
   TextEditingController nomeController = TextEditingController();
 
   TextEditingController pesoController = TextEditingController();
-  String? resultado;
+  String? _infoText = 'Informe seus dados';
   String? imc;
 
-  void resetFields() {
+  void _resetFields() {
     alturaController.text = '';
+    nomeController.text = '';
     pesoController.text = '';
+    setState(() {
+      _infoText = 'Informe seus dados acima para obter seu calculo de IMC';
+    });
     imc = null;
-    resultado = null;
+  }
+
+  _calculate() {
+    setState(() {
+      String nomeDigitado = nomeController.text;
+      double altura = double.parse(alturaController.text) / 100;
+      double peso = double.parse(pesoController.text);
+
+      double ImcRetorno = peso / (altura * altura);
+      if (ImcRetorno < 18.6) {
+        _infoText =
+            'O  usuário: $nomeDigitado, está Abaixo do peso, seu IMC é ${ImcRetorno.toStringAsFixed(2)}';
+      } else if (ImcRetorno >= 18.6 && ImcRetorno < 24.9) {
+        _infoText =
+            'O  usuário: $nomeDigitado, está com Peso Ideal, seu IMC é ${ImcRetorno.toStringAsFixed(2)}';
+      } else if (ImcRetorno >= 24.9 && ImcRetorno < 29.9) {
+        _infoText =
+            'O  usuário: $nomeDigitado, está Levemente Acima do Peso, seu IMC é  ${ImcRetorno.toStringAsFixed(2)}';
+      } else if (ImcRetorno >= 29.9 && ImcRetorno < 34.9) {
+        _infoText =
+            'O  usuário: $nomeDigitado, está com Obesidade Grau I, seu IMC é  ${ImcRetorno.toStringAsFixed(2)}';
+      } else if (ImcRetorno >= 34.9 && ImcRetorno < 39.9) {
+        _infoText =
+            'O  usuário: $nomeDigitado, com Obesidade Grau II, seu IMC é  ${ImcRetorno.toStringAsFixed(2)}';
+      } else if (ImcRetorno > 40) {
+        _infoText =
+            'O  usuário: $nomeDigitado, com Obesidade Grau III, seu IMC é  ${ImcRetorno.toStringAsFixed(2)}';
+      }
+      print(_infoText);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Calculadora de IMC'),
-        ),
-        body: SingleChildScrollView(
+        child: Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculadora de IMC'),
+        actions: <Widget>[
+          IconButton(onPressed: _resetFields, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
           child: Container(
-            color: const Color(0xffFFFFFF),
+            color: Color.fromARGB(255, 255, 255, 255),
             child: Column(
               children: [
+                Container(
+                  child: Image.network(
+                    'https://www.tjdft.jus.br/informacoes/programas-projetos-e-acoes/pro-vida/dicas-de-saude/pilulas-de-saude/o-que-o-indice-de-massa-corporal-imc-diz-sobre-sua-saude/@@images/6b4a3003-7224-4a8c-b001-14e6f2ecca9f.png',
+                  ),
+                ),
                 const SizedBox(
                   height: 50,
-                ),
-                const Text(
-                  '',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                          height: 50,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              color: const Color(0xff879BA2),
-                              shape: BoxShape.rectangle,
-                              borderRadius: BorderRadius.circular(8)),
-                          child: const Row(
-                            children: [
-                              Icon(
-                                Icons.male,
-                                color: Color(0xffC8D8DF),
-                                size: 45,
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                'Homem',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xffC8D8DF),
-                                ),
-                              )
-                            ],
-                          )),
-                    ),
-                    const SizedBox(
-                      width: 30,
-                    ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                          margin: const EdgeInsets.only(right: 20),
-                          height: 50,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              color: const Color(0xffECAB9A),
-                              shape: BoxShape.rectangle,
-                              borderRadius: BorderRadius.circular(8)),
-                          child: const Row(
-                            children: [
-                              Icon(
-                                Icons.female,
-                                size: 45,
-                                color: Color(0xffC8D8DF),
-                              ),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Text(
-                                'Mulher',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xffC8D8DF),
-                                ),
-                              ),
-                            ],
-                          )),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
                 ),
                 Container(
                   margin: const EdgeInsets.all(10),
@@ -132,6 +93,7 @@ class _HomePageState extends State<HomePage> {
                 Container(
                   margin: const EdgeInsets.all(10),
                   child: TextField(
+                    keyboardType: TextInputType.number,
                     controller: alturaController,
                     decoration: const InputDecoration(
                       suffixText: 'Ex. 177',
@@ -143,6 +105,7 @@ class _HomePageState extends State<HomePage> {
                 Container(
                   margin: const EdgeInsets.all(10),
                   child: TextField(
+                    keyboardType: TextInputType.number,
                     controller: pesoController,
                     decoration: const InputDecoration(
                       hintText: 'Peso KG',
@@ -152,96 +115,33 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
                 Container(
-                  height: 20,
-                  child: Text('$resultado'),
-                ),
-                Container(
                   margin: const EdgeInsets.fromLTRB(20, 20, 8, 20),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Container(
-                        width: 200,
-                        height: 75,
-                        decoration: BoxDecoration(
-                            color: const Color(0xff879BA2),
-                            borderRadius: BorderRadius.circular(100)),
-                        margin: const EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                setState(() {
-                                  resetFields();
-                                });
-                              },
-                              child: const Row(
-                                children: [Icon(Icons.refresh), Text('Apagar')],
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                          ],
+                      Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                        child: ElevatedButton(
+                          onPressed: _calculate,
+                          child: Text('Calcular'),
                         ),
                       ),
-                      Container(
-                        width: 130,
-                        height: 75,
-                        decoration: BoxDecoration(
-                            color: const Color(0xffECAB9A),
-                            borderRadius: BorderRadius.circular(100)),
-                        margin: const EdgeInsets.all(8),
-                        child: Row(
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                double? peso =
-                                    double.tryParse(pesoController.text);
-                                double? altura =
-                                    double.tryParse(alturaController.text);
-
-                                if (peso != null && altura != null) {
-                                  altura = altura / 100.0;
-                                  double _imc = (peso / (altura * altura))
-                                      .roundToDouble();
-
-                                  resultado =
-                                      'Digite seus dados para obter o cálculo';
-
-                                  setState(() {
-                                    imc = _imc.toString();
-                                    if (nomeController == '' &&
-                                        alturaController == '' &&
-                                        pesoController == '') {
-                                      resultado = ' Digite seus dados. ';
-                                    } else {
-                                      resultado =
-                                          'O resultado do IMC de ${nomeController.text} é $imc';
-                                    }
-                                  });
-                                }
-                              },
-                              child: const Row(
-                                children: [
-                                  Icon(Icons.forward),
-                                  Text('Calcular')
-                                ],
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 50,
-                            ),
-                          ],
-                        ),
+                      const SizedBox(
+                        width: 50,
                       ),
                     ],
                   ),
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('$_infoText'),
                 ),
               ],
             ),
           ),
         ),
       ),
-    );
+    ));
   }
 }
